6c9ae61aa1a58be86838fb430d753c00f4e63982
devsaji13 <it26102122@my.sliit.lk>
2026-06-20 22:05:34 +0530

    Add files via upload

---
+++ Q2/IT26102122Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT26102122Lab2Q2 {
+    public static void main(String[] args) {
+               double sideLength = 10.0;
+        double pi = 22.0 / 7.0;         
+       
+        double rLength = 4 * sideLength; 
+        
+               double radius = ropeLength / (2 * pi);
+
+        System.out.println("Radius of the circular fence: " + radius);
+    }
+}
